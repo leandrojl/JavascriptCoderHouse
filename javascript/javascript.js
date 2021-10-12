b4e5d361a1eff99aa43fd8b7900b4374bcef9e69
leandrojl <leandroracing96@hotmail.com>
2021-10-12 20:54:52 -0300

    Javascript events

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,124 @@
+let arrayCars = [];
 
+document.getElementById("buttonArray1").onclick = function() {
+    let question = prompt("How many cars you want to put inside the array?");
+    let questionNumber = parseInt(question);
+
+    for(let i=0; i <= questionNumber - 1 ; i++ ){
+        let usuario = prompt("Add a car");
+        arrayCars.push(usuario);
+        console.log(arrayCars[i]);
+    }
+
+    
+ };
+
+ document.getElementById("buttonArray2").onclick = function() {
+    let arrayNames= [];
+    let question1 = prompt("Length of array?")
+    do{
+        let question = prompt("Enter a name:");
+        arrayNames.push(question);
+        console.log(question);
+    } while (arrayNames.length != question1);
+    
+    console.log(arrayNames);
+    
+ };
+
+/* document.getElementById("buttonArray3").onclick = function() {
+    let arrayNames= [];
+    let question1 = prompt("Length of array?")
+    do{
+        let question = prompt("Enter a name:");
+        arrayNames.push(question);
+        console.log(question);
+    } while (arrayNames.length != question1);
+    
+    console.log(arrayNames);
+    
+ };*/
+
+function askInput(){
+    let numberOne = parseInt(prompt("Enter first number:"));
+    let numberTwo = parseInt(prompt("Enter second number:"));
+    var result = (numberOne + numberTwo)/2;
+
+    return result;
+}
+function showConsole(info){
+    console.log(info);
+}
+function alertMessage(message){
+    alert(message);
+}
+// const alertMessage = (message) =>{
+// alert(message);
+//}
+document.getElementById("buttonNumber").onclick = function() {
+    
+    let number = askInput();
+
+
+    document.getElementById("container__averageNumbers").innerHTML = "The average is " +number;
+    showConsole("The average is " +number);
+    alertMessage("The average is " +number);
+    
+    
+
+};
+
+const CAR_ONE ={
+    type: "Ferrari",
+    color: "Blue",
+    model: "Enzo",
+
+    startEngine(){
+        alert("Engine is on!");
+    },
+    stopEngine(){
+        alert("Engine is off!");
+    }
+};
+
+function car(type, color, model){
+    this.type = type;
+    this.color = color;
+    this.model = model;
+
+}
+const CAR_TWO = new car("Renault","Red", "Clio");
+const CAR_THREE = new car("Chevrolet","Black", "Onyx");
+
+// let propertyCar = prompt("Enter property you would like to know;");
+// alert(CAR_ONE[propertyCar]);
+
+// let askCar_Color= prompt("color"); let askCar_type= prompt("type"); let askCar_model= prompt("type");
+
+// for (const propiedad in CAR_ONE){
+// console.log(CAR_ONE[propiedad])   
+// }
+document.getElementById("buttonStartEngine1").onclick = function() {
+    CAR_ONE.startEngine();
+    
+};
+
+document.getElementById("buttonStopEngine1").onclick = function() {
+    CAR_ONE.stopEngine();
+    
+};
+document.getElementById("buttonCarColor").onclick = function() {
+    alert(CAR_ONE.color);
+    
+};
+document.getElementById("buttonCarType").onclick = function() {
+    alert(CAR_ONE.type);
+    
+};
+document.getElementById("buttonCarModel").onclick = function() {
+    alert(CAR_ONE.model);
+    
+};
 document.getElementById("button1").addEventListener("click", function() {
     var person = prompt("Please enter your name");
 
--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,122 +1,73 @@
-let arrayCars = [];
+let precioElementos = document.getElementsByClassName("precio");
 
-document.getElementById("buttonArray1").onclick = function() {
-    let question = prompt("How many cars you want to put inside the array?");
-    let questionNumber = parseInt(question);
+for (precio of precioElementos){
+    let prefijo = document.createElement("span");
 
-    for(let i=0; i <= questionNumber - 1 ; i++ ){
-        let usuario = prompt("Add a car");
-        arrayCars.push(usuario);
-        console.log(arrayCars[i]);
-    }
+    prefijo.innerHTML = "US$";
 
-    
- };
+    precio.prepend(prefijo);
 
- document.getElementById("buttonArray2").onclick = function() {
-    let arrayNames= [];
-    let question1 = prompt("Length of array?")
-    do{
-        let question = prompt("Enter a name:");
-        arrayNames.push(question);
-        console.log(question);
-    } while (arrayNames.length != question1);
-    
-    console.log(arrayNames);
-    
- };
-
- document.getElementById("buttonArray3").onclick = function() {
-    let arrayNames= [];
-    let question1 = prompt("Length of array?")
-    do{
-        let question = prompt("Enter a name:");
-        arrayNames.push(question);
-        console.log(question);
-    } while (arrayNames.length != question1);
-    
-    console.log(arrayNames);
-    
- };
-
-function askInput(){
-    let numberOne = parseInt(prompt("Enter first number:"));
-    let numberTwo = parseInt(prompt("Enter second number:"));
-    var result = (numberOne + numberTwo)/2;
-
-    return result;
 }
-function showConsole(info){
-    console.log(info);
-}
-function alertMessage(message){
-    alert(message);
-}
-// const alertMessage = (message) =>{
-// alert(message);
-//}
-document.getElementById("buttonNumber").onclick = function() {
-    
-    let number = askInput();
 
 
-    document.getElementById("container__averageNumbers").innerHTML = "The average is " +number;
-    showConsole("The average is " +number);
-    alertMessage("The average is " +number);
-    
-    
+let cars =[
 
-};
-
-const CAR_ONE ={
-    type: "Ferrari",
-    color: "Blue",
-    model: "Enzo",
-
-    startEngine(){
-        alert("Engine is on!");
+    {
+        name: "AUDI",
+        color: "blue",
+        src: "/images/audi-blue.jpeg", 
+        model: "a8",
+        price: 15000,
+        id: 1
     },
-    stopEngine(){
-        alert("Engine is off!");
-    }
-};
+    {
+        name: "MERCEDES BENZ",
+        color: "Blue",
+        src: "/images/mercedes-blue.jpeg",
+        model: "a100",
+        price: 17000,
+        id: 2
+    },
+    {
+        name: "FORD",
+        color: "Black",
+        src: "/images/ford-gray.jpeg",
+        model: "Fiesta",
+        price: 5000,
+        id: 3
+    },
+    {
+        name: "RANGE ROVER",
+        color: "White",
+        src: "/images/rover-white.jpeg",
+        model: "4x4",
+        price: 25000,
+        id: 4
+    },
+]
 
-function car(type, color, model){
-    this.type = type;
-    this.color = color;
-    this.model = model;
+let buttonAddCars = document.getElementById("addCar");
+const shopContainer = document.getElementById("shop");
 
-}
-const CAR_TWO = new car("Renault","Red", "Clio");
-const CAR_THREE = new car("Chevrolet","Black", "Onyx");
+const addCars = () =>{
 
-// let propertyCar = prompt("Enter property you would like to know;");
-// alert(CAR_ONE[propertyCar]);
+buttonAddCars.style.backgroundColor = "red";
 
-// let askCar_Color= prompt("color"); let askCar_type= prompt("type"); let askCar_model= prompt("type");
-
-// for (const propiedad in CAR_ONE){
-// console.log(CAR_ONE[propiedad])   
-// }
-document.getElementById("buttonStartEngine1").onclick = function() {
-    CAR_ONE.startEngine();
-    
-};
-
-document.getElementById("buttonStopEngine1").onclick = function() {
-    CAR_ONE.stopEngine();
-    
-};
-document.getElementById("buttonCarColor").onclick = function() {
-    alert(CAR_ONE.color);
-    
-};
-document.getElementById("buttonCarType").onclick = function() {
-    alert(CAR_ONE.type);
-    
-};
-document.getElementById("buttonCarModel").onclick = function() {
-    alert(CAR_ONE.model);
-    
-};
-
+for (let car of cars){
+    let container = document.createElement("div");
+    container.className= "col-md-3 col-xs-12 mt-3";
+    container.innerHTML = 
+    `<div class="card" style="width: 18rem;">
+        <img src="${car.src}" class="card-img-top" alt="...">
+      <div class="card-body text-center">
+            <h5 class="card-title mb-3 font-weight-bold">${car.name}</h5>
+            <p class="card-text">COLOR: ${car.color}</p>
+            <p class="card-text">MODEL: ${car.model}</p>
+            <p class="precio">PRICE: US$${car.price}</p>
+            <a href="#" class="btn btn-primary">BUY</a>
+      </div>
+    </div>`
+    shopContainer.appendChild(container);
+}}
+buttonAddCars.addEventListener("click", addCars);
+buttonAddCars.addEventListener("mousedown", function(){buttonAddCars.style.backgroundColor = "blue";})
