e0d454b7d0497aa4c177b73982513b986ee69b0c
leandrojl <leandroracing96@hotmail.com>
2021-10-21 19:25:16 -0300

    Jquery

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,4 +1,7 @@
-let precioElementos = document.getElementsByClassName("precio");
+jQuery(function(){console.log("DOM ready")
+/* $(document).ready(function(){console.log("DOM ready")}); */
+
+/*let precioElementos = document.getElementsByClassName("precio");
 
 for (precio of precioElementos){
     let prefijo = document.createElement("span");
@@ -7,7 +10,7 @@ for (precio of precioElementos){
 
     precio.prepend(prefijo);
 
-}
+}*/
 
 
 let cars =[
@@ -48,21 +51,37 @@ let cars =[
 
 
 var jsonStringCars = JSON.stringify(cars);
-console.log(jsonStringCars);
+// console.log(jsonStringCars);
 
 var jsonParseCars = JSON.parse(jsonStringCars);
 
-console.log(jsonParseCars);
+// console.log(jsonParseCars);
 
-let buttonAddCars = document.getElementById("addCar");
-const shopContainer = document.getElementById("shop");
+$("#mainTitle").html("CAR SHOP");
+
+let buttonAddCars = $("#addCar")[0];
+const shopContainer = $("#shop")[0]; /*document.getElementById("shop");*/
 
 const addCars = () =>{
 
-buttonAddCars.style.backgroundColor = "red";
+
 
 for (let car of jsonParseCars){
-    let container = document.createElement("div");
+
+$("#shop").append(` <div class="col-md-3 col-xs-12 mt-3">
+                    <div class="card bg-info" style="width: 18rem;">
+                    <img src="${car.src}" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title mb-3 font-weight-bold">${car.name}</h5>
+                        <p class="card-text">COLOR: ${car.color}</p>
+                        <p class="card-text">MODEL: ${car.model}</p>
+                        <p class="precio">PRICE: US$${car.price}</p>
+                        <a href="#" class="btn btn-danger">BUY</a>
+                    </div>
+                    </div>
+                    </div>`);
+
+   /* let container = document.createElement("div");
     container.className= "col-md-3 col-xs-12 mt-3";
     container.innerHTML = 
     `<div class="card" style="width: 18rem;">
@@ -75,23 +94,30 @@ for (let car of jsonParseCars){
             <a href="#" class="btn btn-primary">BUY</a>
       </div>
     </div>`
-    shopContainer.appendChild(container);
+    shopContainer.appendChild(container);*/
 }}
-buttonAddCars.addEventListener("click", addCars);
-buttonAddCars.addEventListener("mousedown", function(){buttonAddCars.style.backgroundColor = "blue";})
+
+$("#addCar").on("click", addCars);
+/*buttonAddCars.addEventListener("click", addCars);*/
+$("#addCar").on("mousedown", function(){buttonAddCars.style.backgroundColor = "blue";} );
+$("#addCar").on("mouseup", function(){buttonAddCars.style.backgroundColor = "#0dcaf0";} );
+// buttonAddCars.addEventListener("mousedown", function(){buttonAddCars.style.backgroundColor = "blue";})
 
 
-let buttonRemoveCars = document.getElementById("removeCars");
+let buttonRemoveCars = $("#removeCars")[0];
+
 
 const removeCars = () =>{
+    $("#removeCars").on("mousedown", function(){buttonRemoveCars.style.backgroundColor = "blue";} );
+$("#removeCars").on("mouseup", function(){buttonRemoveCars.style.backgroundColor = "#0dcaf0";} );
 /*shopContainer.removeChild(shopContainer.childNodes);*/
-shopContainer.textContent = '';
+$("#shop").html("");
 }
+$("#removeCars").click(removeCars);
 
-buttonRemoveCars.addEventListener("click", removeCars);
+// buttonRemoveCars.addEventListener("click", removeCars);
 
-let buttonTermsAndConditions = document.getElementById("buttonTerms&Conditions");
-
+// let buttonTermsAndConditions = $("#buttonTerms&Conditions");
 
 
 const acceptance = () =>{
@@ -107,5 +133,6 @@ if(acceptTermsAndConditions === null || acceptTermsAndConditions === false || ac
     }; 
 } 
 }
-
-buttonTermsAndConditions.addEventListener("click", acceptance);
+$("#buttonTermsAndConditions").on("click", acceptance);
+//buttonTermsAndConditions.addEventListener("click", acceptance);
+});
