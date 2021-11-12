64b8f5eb6b228c850cdfb944b5b8d6a9526290f1
leandrojl <leandroracing96@hotmail.com>
2021-11-12 16:44:45 -0300

    Fixing css, js and json

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,81 +1,33 @@
+jQuery(function(){
+    
+console.log("DOM ready");
 
-/*var jsonStringCars = JSON.stringify(cars);
-// console.log(jsonStringCars);
+//MAIN TITLE
+$("#mainTitle").html("CAR SHOP");
 
-var jsonParseCars = JSON.parse(jsonStringCars);*/
-
-// console.log(jsonParseCars);
-
-jQuery(function(){console.log("DOM ready")
-
-const URL_API = "https://api.mercadolibre.com/sites/MLA/search?q=autos#json";
-
-function bringApi(){
-    $.get(URL_API, (answer, status)=>{
-        console.log(answer);
-        
-        console.log(status);
-        for(let i= 0; i < 50; i++){
-        if(status === "success"){
-            $("#shop").append(` <div class="col-md-3 col-xs-12">
-                                <div class="card bg-info m-1">
-                                <img src="${answer.results[i].thumbnail}" class="img-fluid" alt="...">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-3 font-weight-bold">${answer.results[i].title}</h5>
-                                    <p class="card-text">PRICE: $ ${answer.results[i].price}</p>
-                                    
-                                    <p class="precio">SELLER: <a href="${answer.results[i].seller.permalink}">${answer.results[i].seller.permalink}</a></p>
-                                    <a href="#" class="btn btn-danger">BUY</a>
-                                </div>
-                                </div>
-                                </div>`);
-        }}
-    });
-}
-
+//JSON CARS STORAGE
 const URL = "../json/storage.json";
 
-$("#mainTitle").html("CAR SHOP");
-$("#callApi").on("click", bringApi);
-
-let buttonAddCars = $("#addCar")[0];
-
-const shopContainer = $("#shop")[0]; 
-
-const addCars = () =>{
 $.get(URL, (answer, status) =>{
     console.log(answer);
     console.log(status);
     let cars = answer;
     if (status === "success"){
         for (let car of cars){
-            $("#shop").append(` <div class="col-md-3 col-xs-12">
-                                <div class="card bg-info m-1">
+            $("#shop").append(` <div class="col-md-3 col-xs-12 mb-3">
+                                <div class="card bg-white m-1">
                                 <img src="${car.src}" class="card-img-top" alt="...">
                                 <div class="card-body text-center">
                                     <h5 class="card-title mb-3 font-weight-bold">${car.name}</h5>
                                     <p class="card-text">COLOR: ${car.color}</p>
                                     <p class="card-text">MODEL: ${car.model}</p>
-                                    <p class="precio">PRICE: US$${car.price}</p>
-                                    <a href="#" class="btn btn-danger">BUY</a>
+                                    <p class="precio">PRICE: US$ ${car.price}</p>
+                                    <a href="#" class="btn btn-dark">BUY</a>
                                 </div>
                                 </div>
-                                </div>`);}}})}
-$("#addCar").on("click", addCars);
+                                </div>`)}}})
 
-$("#addCar").on("mousedown", function(){buttonAddCars.style.backgroundColor = "blue";} );
-$("#addCar").on("mouseup", function(){buttonAddCars.style.backgroundColor = "#0dcaf0";} );
-
-let buttonRemoveCars = $("#removeCars")[0];
-
-const removeCars = () =>{
-    $("#removeCars").on("mousedown", function(){buttonRemoveCars.style.backgroundColor = "blue";} );
-$("#removeCars").on("mouseup", function(){buttonRemoveCars.style.backgroundColor = "#0dcaf0";} );
-
-$("#shop").html("");
-}
-$("#removeCars").click(removeCars);
-
+//TERMS AND CONDIIONS 
 const acceptance = () =>{
 
 const acceptTermsAndConditions = JSON.parse(localStorage.getItem("termsAndConditions"));
